b3f9190555b35bb0014964d69b54c1e63057fb21
DecksDark <patrickdiazgrandez@gmail.com>
2025-07-06 11:01:54 +0200

    cleaup before installing LSP

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -5,8 +5,8 @@ return {
 	config = function()
 		local builtin = require('telescope.builtin')
 		-- find files in current directory
-		vim.keymap.set('n', '<leader>fp', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 		-- fuzzyfinds for a String in current diretory
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+		vim.keymap.set('n', '<leader>gg', builtin.live_grep, { desc = 'Telescope live grep' })
 	end
 }
